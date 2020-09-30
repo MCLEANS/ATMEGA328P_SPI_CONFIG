b074d3596b98eace10e63b254aea1b8d51f43bce
MCLEANS <jackagwaya@gmail.com>
2020-09-30 11:11:42 +0300

    implemented SPI library

--- main.cpp
+++ main.cpp
@@ -9,10 +9,20 @@
 #include <util/delay.h>
 #include "SPI.h"
 
+custom_libraries::SPI spi(2,false,false,false);
+
 int main(void){
 
+	//configure chip select pin
+	DDRB |= (1 << PIN2);
+	PORTB |= (1<<PIN2);
+
+	PORTB &= ~(1 << PIN2);
+	spi.write('S');
+	PORTB |= (1<< PIN2);
+
 	while (1){
-		
+
 	}
 }
 
